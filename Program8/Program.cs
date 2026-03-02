717628c6e4d601f4b249ab9b39ad33f80a5393a9
nirajvala125518 <niraj.vala125518@marwadiuniversity.ac.in>
2026-03-02 10:39:58 +0530

    Add files via upload

---
+++ Program8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int n = int.Parse(Console.ReadLine());
+            int fact = 1;
+            for (int i = n; i >= 1; i--)
+            {
+                fact = fact *i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
