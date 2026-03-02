7667db384eda33155cea63b7d84d5ac3745a725e
nirajvala125518 <niraj.vala125518@marwadiuniversity.ac.in>
2026-03-02 10:34:04 +0530

    Add files via upload

---
+++ Program3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float p, r, n, i;
+
+            Console.WriteLine("Enter value of Principle:");
+             p = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter  rate value ");
+            r = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter year value");
+             n = float.Parse(Console.ReadLine());
+            i = p * r * n;
+            i = i / 100;
+            Console.WriteLine("simple interest" + i);  
+
+        }
+    }
+}
