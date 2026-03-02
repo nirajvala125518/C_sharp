3f48a90da48370dcd97de913ced8544f943fc661
nirajvala125518 <niraj.vala125518@marwadiuniversity.ac.in>
2026-03-02 10:39:16 +0530

    Add files via upload

---
+++ Program7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Age");
+            int a = int.Parse(Console.ReadLine());
+            if (a < 12)
+            {
+                Console.WriteLine("you are a kid");
+            }
+            else if (a >= 12 && a <= 17)
+            {
+                Console.WriteLine("Teenager");
+            }
+            else if (a >= 18 && a <= 60)
+            {
+                Console.WriteLine("you are a adult");
+            }
+            else
+            {
+                Console.WriteLine("you are a senior citizen");
+            }
+        }
+    }
+
+}
