bcd0aee33cdfea2e77bed9eb05998f6aa05c9ac2
nirajvala125518 <niraj.vala125518@marwadiuniversity.ac.in>
2026-04-14 23:01:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace u3p12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int max, i;
+
+            Console.WriteLine("Enter 5 numbers:");
+
+            for (i = 0; i < 5; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            max = arr[0];
+
+            for (i = 1; i < 5; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            Console.WriteLine("Maximum number is: " + max);
+        }
+
+    }
+}
