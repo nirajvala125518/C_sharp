4b3f604c189f66ee3ba57bb50df33c5a68fba01d
nirajvala125518 <niraj.vala125518@marwadiuniversity.ac.in>
2026-04-14 23:11:47 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,15 +5,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace emp
+namespace jagged_array
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            detalis d = new detalis();
-            d.emp_detalis();
-            d.display();
+            int[][] j1 = new int[3][];
+
+            j1[0] = new int[3];
+            j1[1] = new int[4];
+            j1[2] = new int[1];
+
+            for (int i = 0; i < j1.Length; i++)
+            {
+                for (int j = 0; j < j1[i].Length; j++)
+                {
+                    Console.WriteLine("enter value ");
+                    j1[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("value are ......");
+
+            for (int i = 0; i < j1.Length; i++)
+            {
+                for (int j = 0; j < j1[i].Length; j++)
+                {
+                    Console.Write(j1[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
