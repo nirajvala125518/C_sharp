627a22898a13374df713e7bdb38570b3d86cf8b5
nirajvala125518 <niraj.vala125518@marwadiuniversity.ac.in>
2026-04-14 23:09:02 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,19 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace constructor
+namespace emp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Rectangle r1 = new Rectangle();
-            r1.calculate();
-            r1.Display();
-
-            Rectangle r2 = new Rectangle(30, 40);
-            r2.calculate();
-            r2.Display();
+            detalis d = new detalis();
+            d.emp_detalis();
+            d.display();
         }
     }
 }
---
+++ detalis.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emp
+{
+    internal class detalis
+    {
+        int eid;
+        int econ;
+        String ename;
+        public void emp_detalis()
+        {
+            Console.WriteLine("Enter Employee Name  --> ");
+            ename = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee Contact Number --> ");
+            econ = (int)Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee id--> ");
+            eid = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Employee Name is -->" + ename);
+            Console.WriteLine("Employee Contact Number  is -->" + econ);
+            Console.WriteLine("Employee Id is -->" + eid);
+        }
+
+    }
+}
